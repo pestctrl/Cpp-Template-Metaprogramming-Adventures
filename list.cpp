75578d2ef6526bc479708b1d06712f20664d1355
Benson Chu <bensonchu457@gmail.com>
2023-05-14 07:25:16 -0500

    WE HAVE LISTS

--- list.cpp
+++ list.cpp
@@ -5,7 +5,34 @@
 void list_asserts()
 {
   Cons<GenNumber<3>::result,
-       GenNumber<2>::result> typedef cons;
-  assert((Car<cons>::result::value == 3));
-  assert((Cdr<cons>::result::value == 2));
+       GenNumber<2>::result> typedef cons1;
+
+  assert((Car<cons1>::result::value == 3));
+  assert((Cdr<cons1>::result::value == 2));
+
+  Cons<GenNumber<4>::result,
+       Cons<GenNumber<3>::result,
+            GenNumber<2>::result>> typedef cons2;
+
+  assert(Car<cons2>::result::value == 4);
+  assert(Car<Cdr<cons2>::result>::result::value == 3);
+  assert(Cdr<Cdr<cons2>::result>::result::value == 2);
+
+  List<> typedef list1;
+
+  assert(list1::result::value == NIL);
+
+  List<GenNumber<4>::result>::result typedef list2;
+
+  assert(Car<list2>::result::value == 4);
+  assert(Cdr<list2>::result::value == NIL);
+
+  List<GenNumber<4>::result,
+       GenNumber<3>::result,
+       GenNumber<2>::result>::result typedef list3;
+
+  assert(Car<list3>::result::value == 4);
+  assert(Car<Cdr<list3>::result>::result::value == 3);
+  assert(Car<Cdr<Cdr<list3>::result>::result>::result::value == 2);
+  assert(Cdr<Cdr<Cdr<list3>::result>::result>::result::value == NIL);
 }
--- list.h
+++ list.h
@@ -1,6 +1,15 @@
+#include <climits>
+
 // Values
 template<typename I, typename J>
 struct Cons { };
+
+#define NIL INT_MIN + 1
+
+struct Nil
+{
+  enum { value = INT_MIN + 1 };
+};
 
 // Functions
 template<typename I>
@@ -19,6 +28,27 @@ template <typename I, typename J>
 struct Cdr<Cons<I,J>>
 {
   J typedef result;
+};
+
+template<typename... Ts>
+struct List { };
+
+template <>
+struct List<>
+{
+  Nil typedef result;
+};
+
+template <typename T>
+struct List<T>
+{
+  Cons<T, Nil> typedef result;
+};
+
+template <typename T, typename... Ts>
+struct List<T, Ts...>
+{
+  Cons<T,typename List<Ts...>::result> typedef result;
 };
 
 void list_asserts();
