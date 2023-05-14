daaa498ccd9cf8640cfd00774c3a892caab89777
Benson Chu <bensonchu457@gmail.com>
2023-05-13 19:53:31 -0500

    Factorials!

--- main.cpp
+++ main.cpp
@@ -13,12 +13,28 @@ struct Succ
   enum { value = N::value + 1 };
 };
 
+template<int N>
+struct Factorial
+{
+  enum { value = N * Factorial<N-1>::value };
+};
+
+template<>
+struct Factorial<0>
+{
+  enum { value = 1 };
+};
+
 int main() {
   assert(true);
 
   assert(Zero::value == 0);
 
   assert(Succ<Zero>::value == 1);
+
+  assert(Factorial<0>::value == 1);
+
+  assert(Factorial<3>::value == 6);
 
   std::cout << "SUCCESS!" << std::endl;
 }
