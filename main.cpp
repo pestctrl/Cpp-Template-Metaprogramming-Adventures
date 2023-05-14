3077c9cff0fe64582f3e83d1b4280a909a67c10d
Benson Chu <bensonchu457@gmail.com>
2023-05-13 19:53:00 -0500

    And thus, we have numbers

--- main.cpp
+++ main.cpp
@@ -7,10 +7,18 @@ struct Zero
   enum { value = 0 };
 };
 
+template<typename N>
+struct Succ
+{
+  enum { value = N::value + 1 };
+};
+
 int main() {
   assert(true);
 
   assert(Zero::value == 0);
+
+  assert(Succ<Zero>::value == 1);
 
   std::cout << "SUCCESS!" << std::endl;
 }
